8352671b7a9d826f8a385d737e105928b9b3ed11
Pedro Henrique Goes de Oliveira <pedrohenriquesk10@yahoo.com>
2020-08-07 19:08:57 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const proffys = [
     {
-         name: "Carlos Henrique", 
+         name: "Pedro Henrique", 
          avatar: "https://avatars1.githubusercontent.com/u/68116354?v=4", whatsapp: "35998587192",
-         bio: "Entusiasta das melhores tecnologias de matemática avançada.<br><br>Apaixonado por explodir raciocínios e por mudar a vida das pessoas através do pensamento. Menos de 1 pessoa já passou por um de meus raciocínios.", 
+         bio: "Entusiasta das melhores tecnologias de matemática avançada. Apaixonado por explodir raciocínios e por mudar a vida das pessoas através do pensamento. Menos de 1 pessoa já passou por um de meus raciocínios.", 
          subject: "Matemática", 
          cost: "20", 
          weekday: [0], 
@@ -12,7 +12,7 @@ const proffys = [
     {
         name: "Daniela Conceição", 
         avatar: "https://avatars1.githubusercontent.com/u/68116354?v=4", whatsapp: "895628565656445",
-        bio: "Entusiasta das melhores tecnologias de matemática avançada.<br><br>Apaixonado por explodir raciocínios e por mudar a vida das pessoas através do pensamento. Menos de 1 pessoa já passou por um de meus raciocínios.", 
+        bio: "Entusiasta das melhores tecnologias de matemática avançada. Apaixonado por explodir raciocínios e por mudar a vida das pessoas através do pensamento. Menos de 1 pessoa já passou por um de meus raciocínios.", 
         subject: "Matemática", 
         cost: "20", 
         weekday: [1], 
