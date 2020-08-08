ed9c19a6a67dd23a8b2736e4bbb1c89ed3801e80
Pedro Henrique Goes de Oliveira <pedrohenriquesk10@yahoo.com>
2020-08-08 20:17:36 -0300

    Update

---
+++ src/database/fake_data.js
@@ -0,0 +1,25 @@
+const proffys = [
+    {
+         name: "Pedro Henrique", 
+         avatar: "https://avatars1.githubusercontent.com/u/68116354?v=4", 
+         whatsapp: "35998587192",
+         bio: "Entusiasta das melhores tecnologias de matemática avançada. Apaixonado por explodir raciocínios e por mudar a vida das pessoas através do pensamento. Menos de 1 pessoa já passou por um de meus raciocínios.", 
+         subject: "Matemática", 
+         cost: "20", 
+         weekday: [0], 
+         time_FormData: [720], 
+         time_to: [1220]
+    },
+    {
+        name: "Daniela Conceição", 
+        avatar: "https://avatars1.githubusercontent.com/u/68116354?v=4", 
+        whatsapp: "895628565656445",
+        bio: "Entusiasta das melhores tecnologias de matemática avançada. Apaixonado por explodir raciocínios e por mudar a vida das pessoas através do pensamento. Menos de 1 pessoa já passou por um de meus raciocínios.", 
+        subject: "Matemática", 
+        cost: "20", 
+        weekday: [1], 
+        time_FormData: [720], 
+        time_to: [1220]
+    }
+]
+
