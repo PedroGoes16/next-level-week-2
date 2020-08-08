c63dd80c411f490dee7ade62eabecb75e2467b1b
Pedro Henrique Goes de Oliveira <pedrohenriquesk10@yahoo.com>
2020-08-08 15:55:05 -0300

    update

--- src/server.js
+++ src/server.js
@@ -21,16 +21,52 @@ const proffys = [
     }
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+]
+
 function pageLanding(req, res) {
     return res.render("index.html")
 }
 
+function getSubject(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
 function pageStudy(req, res) {
-    return res.render("study.html", { proffys })
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    const data = req.query
+    const isNotEmpty = Object.keys(data).length > 0
+    if (isNotEmpty){
+        data.subject = getSubject(data.subject)
+        proffys.push(data)
+        return res.redirect("/study")
+    }
+    return res.render("give-classes.html", { subjects, weekdays })
 }
 
 const express = require('express')
